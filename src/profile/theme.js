61c01d1f9b2c14c0c33dce8df2407db2af83493d
Tay Kai Yang <taykaiyang556@gmail.com>
2021-05-22 13:22:21 +0800

    Updates

--- src/profile/theme.js
+++
@@ -1,8 +0,0 @@
-export const Theme = {
-    block: getComputedStyle(document.documentElement).getPropertyValue("--block"),
-    background: getComputedStyle(document.documentElement).getPropertyValue("--background"),
-    highlight: getComputedStyle(document.documentElement).getPropertyValue("--highlight"),
-    header: getComputedStyle(document.documentElement).getPropertyValue("--header"),
-    muted: getComputedStyle(document.documentElement).getPropertyValue("--muted"),
-    text: getComputedStyle(document.documentElement).getPropertyValue("--text")
-}
